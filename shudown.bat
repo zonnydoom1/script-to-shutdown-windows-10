07cbed25872c4384d09a059b9d3df57e3a4261b5
Mustapha Abdullah <110315911+zonnydoom1@users.noreply.github.com>
2023-01-29 23:54:10 +0000

    Create shudown.bat

---
+++ shudown.bat
@@ -0,0 +1,19 @@
+@ echo off
+color F
+echo Hi guys
+msg * Enter to begin installation
+msg * You are hacked
+msg * Computer isn't responding want to shutdown
+msg * You are fool if you press ok
+msg * Cool thing but you are....
+msg * Cancel it fast or I will shutdown your computer
+msg * You are trapped 
+msg * Your computer will shutdown in 60 seconds
+msg * HA! hahaha
+msg * Your pc is in my control
+msg * Virus is entering in your PC
+msg * Hard disk corrupted computer going to shutdown forever your private info is being gathered
+msg * It will never stop, goodbye you are gonna be hacked
+msg * It is a joke
+timeout 5
+shudown -s
